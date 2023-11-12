bb9a4ab8a057d19ac6048b248fa171f76a250c3f
shaown <34128508+camonanesi123@users.noreply.github.com>
2023-11-12 21:41:08 +0800

    ddd
    ddd

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import background from "./img/unnamed.jpg";
+import background1 from "./img/header1.png";
+import background2 from "./img/header2.png";
+import background3 from "./img/header3.png";
+import background4 from "./img/header4.png";
+import ExchangeRateComponent from './ExchangeRateComponent';
+import React, { useState } from 'react';
 
-function App() {
+const App = () =>{
+  const [appData, setAppData] = useState(null);
+
+  const handleDataReceived = (data) => {
+    setAppData(data); // 在这里处理来自子组件的数据
+  };
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+<div class="container">
+  <div class="item">
+    <img src={background1} alt="Image 1" />
+  </div>
+  <div class="item">
+    <img src={background2} alt="Image 2" />
+    <span className="overlay-text"> <ExchangeRateComponent onDataReceived={handleDataReceived} /></span>
+  </div>
+  <div class="item">
+    <img src={background3} alt="Image 2" />
+    <span className="overlay-text"> {appData !== null && appData !== undefined ? (50 * appData).toFixed(2) : 'N/A'} CNY</span>
+  </div>
+  <div class="item">
+    <img src={background4} alt="Image 2" />
+  </div>
+  </div>
+    </>
   );
 }
 
---
+++ src/ExchangeRateComponent.jsx
@@ -0,0 +1,29 @@
+// ExchangeRateComponent.jsx
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import PropTypes from 'prop-types';
+
+const ExchangeRateComponent = ({ onDataReceived }) => {
+  const [exchangeRate, setExchangeRate] = useState(null);
+
+  useEffect(() => {
+    const apiUrl = '/api'; // 使用代理路径
+
+    axios.get(apiUrl)
+      .then(response => {
+        const usdToCnyRate = response.data.result[0].data1['bankConversionPri'];
+        setExchangeRate(usdToCnyRate*0.01);
+        onDataReceived(usdToCnyRate*0.01);
+      })
+      .catch(error => console.error('获取数据出错:', error));
+  }, [onDataReceived]);
+
+  return (
+        <p>1 USD = {exchangeRate !== null ? (0.98 * exchangeRate).toFixed(2) : 'N/A'} CNY</p>
+  );
+};
+ExchangeRateComponent.propTypes = {
+  onDataReceived: PropTypes.func.isRequired,
+};
+
+export default ExchangeRateComponent;
