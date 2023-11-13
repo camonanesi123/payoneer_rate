d02f04eb80cd5ba433b82e1b80f6e1aa08601b53
shaown <34128508+camonanesi123@users.noreply.github.com>
2023-11-13 10:51:49 +0800

    rtttgs
    srtts

---
+++ src/api/proxy.js
@@ -0,0 +1,14 @@
+const request = require('request');
+
+module.exports = function(app) {
+  app.use(
+    '/api', // 要代理的路径
+    createProxyMiddleware({
+      target: 'http://web.juhe.cn', // 目标API的地址
+      changeOrigin: true,
+      pathRewrite: {
+        '^/api': '/finance/exchange/rmbquot?type=0&bank=3&key=21d1afa89475cfed5d9cc42e069930b6',
+      },
+    })
+  );
+};
